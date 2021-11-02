44c2a3b19914a31b6d3ec3da33a3c49fa2000909
Kieu <kieuphan530@gmail.com>
2021-11-02 00:57:09 -0500

    commit tree a78da9c9b3f5f77f7ed443f2043174112285c51f

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    const wordArray = string.toLowerCase().split(" ");
+    let words = wordArray.map(word => {
+      if (word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but'  && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from'){
+        return word[0].toUpperCase() + word.slice(1)
+      } else {
+        return word
+      }
+    }).join(" ")
+    return words[0].toUpperCase() + words.slice(1)
+  }  
 }
+
+console.log(Formatter.titleize("my name is kathy"))
